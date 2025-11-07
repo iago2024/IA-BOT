6f985b666ec8b9a9fcc05e835336f331619a4b56
iago2024 <154073418+iago2024@users.noreply.github.com>
2025-11-07 03:59:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -223,5 +223,6 @@ def manual_check_mhi_t5_api():
 
 
 # ✅ Final correto para Render e execução local
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
